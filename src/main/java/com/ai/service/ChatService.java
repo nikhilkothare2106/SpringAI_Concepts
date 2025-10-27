3d4cecbea7e2f6bce06c7666188611581e969230
Nikhil Kothare <nikhilkothare12@gmail.com>
2025-10-27 22:43:48 +0530

    RAG

--- src/main/java/com/ai/service/ChatService.java
+++ src/main/java/com/ai/service/ChatService.java
@@ -1,14 +1,21 @@
 package com.ai.service;
 
 import com.ai.advisor.TokenAdvisor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class ChatService {
 
@@ -20,18 +27,37 @@ public class ChatService {
 
 
     private ChatClient chatClient;
+    private VectorStore vectorStore;
 
-    public ChatService(ChatClient chatClient){
+    public ChatService(ChatClient chatClient, VectorStore vectorStore){
         this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
     }
     public String chat(String q,String userId) {
+
+        SearchRequest searchRequest = SearchRequest
+                .builder()
+                .topK(5)
+                .similarityThreshold(0.5)
+                .query(q)
+                .build();
+
+        List<Document> documents = vectorStore.similaritySearch(searchRequest);
+        List<String> list = documents.stream().map(Document::getText).toList();
+        String contextData = String.join(", \n", list);
+
+        log.info("contextData: {}", contextData);
+
         return chatClient
                 .prompt(q)
                 .advisors(advisorSpec -> {
                     advisorSpec.param(ChatMemory.CONVERSATION_ID,userId);
                 })
-//                .system(system->system.text(systemMessage))
-//                .user(user->user.text(userMessage))
+                .advisors(new SimpleLoggerAdvisor())
+                .system(system->system.text(systemMessage)
+                        .param("documents",contextData))
+                .user(user->user.text(userMessage)
+                        .param("query",q))
                 .call()
                 .content();
 
@@ -44,5 +70,13 @@ public class ChatService {
                 .user(user->user.text(userMessage))
                 .stream()
                 .content();
+    }
+
+    public void saveData(List<String> list) {
+
+        List<Document> documentList = list.stream().map(Document::new).toList();
+        this.vectorStore.add(documentList);
+
+
     }
 }
--- src/test/java/com/ai/SpringAi01ApplicationTests.java
+++ src/test/java/com/ai/SpringAi01ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ai;
 
+import com.ai.helper.Helper;
+import com.ai.service.ChatService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringAi01ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ChatService chatService;
+    @Test
+    void saveDataToVectorDatabase(){
+        System.out.println("saving data to database");
+        this.chatService.saveData(Helper.getData());
+        System.out.println("data is saved successfully");
+    }
 
 }
